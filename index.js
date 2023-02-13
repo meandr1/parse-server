cc043b5dd6da9324e7318fbe8023e1e83e4c70ff
Andrii Moiseienko <moyseyenko.av@gmail.com>
2023-02-13 23:19:19 +0200

    parse-server

--- index.js
+++ index.js
@@ -4,19 +4,14 @@
 const express = require('express');
 const ParseServer = require('parse-server').ParseServer;
 const path = require('path');
-const args = process.argv || [];
-const test = args.some(arg => arg.includes('jasmine'));
 
-const databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+const databaseUri = 'mongodb+srv://meandr:Potato@cluster0.pgdlohm.mongodb.net/?retryWrites=true&w=majority';
 
-if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
-}
 const config = {
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri,
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: 'WeatherApp',
+  masterKey: 'lksdjhfg23SDG4*', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
@@ -33,14 +28,13 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
 const mountPath = process.env.PARSE_MOUNT || '/parse';
-if (!test) {
   const api = new ParseServer(config);
   app.use(mountPath, api);
-}
+
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function (req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+  res.status(200).send('I dream of being a website. Please star the parse-server repo on GitHub!');
 });
 
 // There will be a test page available on the /test path of your server url
@@ -50,14 +44,12 @@ app.get('/test', function (req, res) {
 });
 
 const port = process.env.PORT || 1337;
-if (!test) {
   const httpServer = require('http').createServer(app);
   httpServer.listen(port, function () {
     console.log('parse-server-example running on port ' + port + '.');
   });
   // This will enable the Live Query real-time server
   ParseServer.createLiveQueryServer(httpServer);
-}
 
 module.exports = {
   app,
